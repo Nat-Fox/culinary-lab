80cfe05a37feb06b5a084b4a80c5aba04a5a7fc5
Natalia Garrido <natgramirez@outlook.com>
2017-06-14 23:02:39 -0400

    arreglando responsive y cambiando navbar de color

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,21 @@ $(document).ready(function() {
         }
     });
 });
+
+/***************** Función para hacer cambio de navbar con el evento scroll en window ***********************/
+
+function cambioDeNavbar() {
+    var distanciaY = window.pageYOffset || document.documentElement.scrollTop;
+    // La navbar cambia de color al de la sección correspondiente al llegar a ella
+    var navbar = document.getElementById('navbar');
+
+    if (distanciaY > 100) {
+        navbar.classList.add('navbar-color');
+        // Si la navbar vuelve arriba regresar al color original, al logo original y al estilo de invisibilidad.
+    } else {
+        navbar.classList.remove('navbar-color');
+    }
+}
+
+// Evento para el navegador al hacer scroll que detona la función que provoca los cambios de color en la navbar segun la distancia
+window.addEventListener('scroll', cambioDeNavbar);
